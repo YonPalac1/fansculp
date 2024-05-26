95ff7e5ffd5117f950733515464ead79dc9fbca6
FrancoANioi <franconioi92@gmail.com>
2024-05-26 08:15:42 -0300

    Actualización 3.0 - Scroll, Form y retoques

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,38 @@
 // *Carrousel Functions
 
-function App() {}
+function App() { }
 
-    window.onload = function(event) {
-        var app = new App();
-        window.app = app;
+window.onload = function (event) {
+    var app = new App();
+    window.app = app;
+}
+
+App.prototype.processingButton = function (event) {
+    const btn = event.currentTarget;
+    const carrouselList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const carrousel = track.querySelectorAll('.carrousel-img');
+
+    const carrouselWidth = carrousel[0].offsetWidth;
+
+    const trackWidth = track.offsetWidth;
+    const listWidth = carrouselList.offsetWidth;
+
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition, carrouselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carrouselWidth, track);
+
+}
+
+let prevAction = (leftPosition, carrouselWidth, track) => {
+    if (leftPosition > 0) {
+        track.style.left = `${-1 * (leftPosition - carrouselWidth)}px`;
     }
+}
 
-    App.prototype.processingButton = function(event) {
-        const btn = event.currentTarget;
-        const carrouselList = event.currentTarget.parentNode;
-        const track = event.currentTarget.parentNode.querySelector('#track');
-        const carrousel = track.querySelectorAll('.carrousel-img');
-
-        const carrouselWidth = carrousel[0].offsetWidth;
-
-        const trackWidth = track.offsetWidth;
-        const listWidth = carrouselList.offsetWidth;
-
-        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2) * -1);
-        btn.dataset.button == "button-prev" ? prevAction(leftPosition,carrouselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carrouselWidth, track);
-
+let nextAction = (leftPosition, trackWidth, listWidth, carrouselWidth, track) => {
+    if (leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + carrouselWidth)}px`;
     }
-
-    let prevAction = (leftPosition, carrouselWidth, track) => {
-        if (leftPosition > 0) {
-            track.style.left = `${-1 * (leftPosition - carrouselWidth)}px`;
-        }
-    }
-
-    let nextAction = (leftPosition, trackWidth, listWidth, carrouselWidth, track) => {
-        if (leftPosition <  (trackWidth - listWidth)) {
-            track.style.left = `${-1 * (leftPosition + carrouselWidth)}px`;
-        }
 }
 
 // *FIN Carrousel Functions
@@ -82,3 +82,195 @@ function removeModals() {
     modals.classList.remove("openModal")
     nav.classList.remove("visible");
 }
+
+
+
+// ***Funciones de los Filtros*** //
+
+// Traigo los elementos del DOM
+const btns = document.querySelectorAll('.filters button');
+const imgs = document.querySelectorAll('.card img');
+
+// Agrego el click event a todos los botones
+
+for (let i = 1; i < btns.length; i++) {
+    btns[i].addEventListener('click', filterImg);
+}
+
+// Set active button on click
+function setActiveBtn(e) {
+    //Quito la clase activa de los botones
+    btns.forEach(btn => {
+        btn.classList.remove('active-btn'); //Quita la Clase 'active-btn' a todos los botones
+    });
+
+    //Agrego la clase al boton que se le hizo click
+    e.target.classList.add('active-btn');
+}
+
+
+//Filtro las imagenes
+function filterImg(e) {
+    //Corro la funcion activebtn
+    setActiveBtn(e);
+
+    //Recorro todas las imagenes
+    imgs.forEach(img => {
+        // Expando todas las imagenes
+        img.classList.remove('img_shrink');
+        img.classList.add('img_expand');
+
+        // Traigo la data de los data-atributo
+
+        //Traigo el numero de data-img
+        const imgType = parseInt(img.dataset.img);
+
+        //Traigo el numero de data-btn
+        const btnType = parseInt(e.target.dataset.btn);
+
+        /* 
+        Si imgtype y btntype(donde se hizo click)
+        son iguales...
+        */
+
+        if (imgType !== btnType) {
+            // Escondo la imagen
+            img.classList.remove('img_expand');
+            img.classList.add('img_shrink');
+        }
+    });
+}
+
+
+// Establezco el evento al boton 'all'
+btns[0].addEventListener('click', (e) => {
+    // Corro la funcion de activebutton
+    setActiveBtn(e);
+
+    // Recorro todas las imagenes
+    imgs.forEach(img => {
+        // Expando todas las imagenes
+        img.classList.remove('img_shrink');
+        img.classList.add('img_expand');
+    });
+});
+
+
+// Funciones de formularios
+const comments = [{
+    title: "Experiencia increible",
+    comment: "FANSCULP es sin duda el mejor lugar para llevar a cabo tus proyectos de diseño 3d. Desde el primer momento, el equipo me brindo una atención personalizada y profesional, asesoramiento en cada paso del proceso",
+    autor: "Miguel"
+}, {
+    title: "Buena atencion",
+    comment: "FANSCULP es sin duda el mejor lugar para llevar a cabo tus proyectos de diseño 3d. Desde el primer momento, el equipo me brindo una atención personalizada y profesional, asesoramiento en cada paso del proceso",
+    autor: "Jon"
+}, {
+    title: "Rapidisimo",
+    comment: "FANSCULP es sin duda el mejor lugar para llevar a cabo tus proyectos de diseño 3d. Desde el primer momento, el equipo me brindo una atención personalizada y profesional, asesoramiento en cada paso del proceso",
+    autor: "Rupert"
+},]
+const container_comments = document.getElementById("container_comments")
+const dots = document.getElementById("dots")
+const dot = document.getElementsByClassName("dot")
+
+let n_comment = 0;
+
+for (let i = 0; i < comments.length; i++) {
+    dots.innerHTML += `<span class="dot"></span>`
+} 
+
+function changeDots(n_comment) {
+    dot[n_comment].classList.add("active")
+    if (n_comment === 0) {
+        dot[1].classList.remove("active")
+        dot[2].classList.remove("active")
+    } else if (n_comment === 1) {
+        dot[0].classList.remove("active")
+        dot[2].classList.remove("active")
+    }else if (n_comment === 2) {
+        dot[0].classList.remove("active")
+        dot[1].classList.remove("active")
+    }
+}
+
+function changeComments(n_comment) {
+    container_comments.innerHTML = `<div class="comment">
+    <h4>${comments[n_comment].title}</h4>
+    <p>${comments[n_comment].comment}</p>
+    <span>${comments[n_comment].autor}</span>
+    </div>`
+    changeDots(n_comment)
+}
+
+setInterval(() => {
+    if (n_comment < comments.length) {
+        changeComments(n_comment)
+        n_comment = n_comment + 1
+    } else {
+        n_comment = 0
+        changeComments(n_comment)
+    }
+}, [2000])
+
+changeComments(n_comment)
+
+
+// *Validación de formulario
+/*/ const validarFormulario = (idFormulario) => {
+       const listaCampos = document.querySelectorAll(`#${idFormulario} [data-validate]`);
+       let validacion = true;
+       console.log('listaCampos :>> ', listaCampos);
+    if (listaCampos.length > 0) {
+     listaCampos.forEach(elemento => {
+         const tipoElemento = elemento.getAttribute("type");
+         console.log ('tipoElemento :>> ', tipoElemento);
+         if (elemento.value === "") {
+             validacion = false;
+             elemento.style.setProperty("border", "1 px solid red");
+             setTimeout(() => {
+                 elemento.style.setProperty("border", "");
+             }, 2000);
+         }
+         if (tipoElemento === "checkbox" && !elemento.checked) {
+             validacion = false;
+             elemento.style.setProperty("border", "1 px solid red");
+             setTimeout(() => {
+                 elemento.style.setProperty("border", "");
+             }, 2000);
+         })
+       }
+ 
+    } /*/ 
+/*/ } /*/ 
+
+ /*/ const enviarFormulario = () => {
+     event.preventDefault();
+     validarFormulario('form-validation');
+    } /*/ 
+ 
+ 
+ // *FIN Validación de formulario
+ 
+
+ // *Botón hacia Arriba
+ /*/ window.onscroll = function(){
+      console.log(document.documentElement.scrollTop);
+        if(document.documentElement.scrollTop > 100){
+           document.querySelector('.go-top-container') 
+           .classList.add('show');
+        } else {
+         document.querySelector('.go-top-container') 
+         .classList.remove('show');
+        }
+    } /*/
+ 
+ /*/ document.querySelector('.go-top-container') 
+ .addEventListener('click', () => {
+       window.scrollTo ({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }) /*/ 
+ // *FIN Botón hacia Arriba
+ 
